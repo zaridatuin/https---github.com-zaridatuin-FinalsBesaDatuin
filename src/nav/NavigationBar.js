5f78c3fc6b09d1ab5755fb76364ef0bc4e72e29f
Zari <zaritalavera@yahoo.com>
2024-11-22 02:49:55 +0800

    asd

--- src/nav/NavigationBar.js
+++ src/nav/NavigationBar.js
@@ -29,8 +29,6 @@ export default function NavigationBar() {
                     {user && (
                         <Nav className="me-auto">
                             <NavLink to="/"><div className="nav-button">Home</div></NavLink>
-                            <NavLink to="/about"><div className="nav-button">About</div></NavLink>
-                            <NavLink to="/contact"><div className="nav-button">Contact</div></NavLink>
                             <NavLink to="/menu"><div className="nav-button">Menu</div></NavLink>
                             <NavLink to="/reservation"><div className="nav-button">Reservation</div></NavLink>
                             <NavLink to="/feedback"><div className="nav-button">Feedback</div></NavLink>
@@ -56,53 +54,5 @@ export default function NavigationBar() {
         </Navbar>
 
         </>
-        // <nav className="navbar navbar-expand-lg custom-navbar">
-        //     <div className="container">
-        //         <NavLink className="navbar-brand" to="/">
-        //             <strong>Restaurant Name</strong>
-        //         </NavLink>
-        //         <button
-        //             className="navbar-toggler"
-        //             type="button"
-        //             data-bs-toggle="collapse"
-        //             data-bs-target="#navbarNav"
-        //             aria-controls="navbarNav"
-        //             aria-expanded="false"
-        //             aria-label="Toggle navigation"
-        //         >
-        //         <span className="navbar-toggler-icon"></span>
-        //         </button>
-
-        //         <div className="collapse navbar-collapse" id="navbarNav">
-        //             <ul className="navbar-nav ms-auto">
-        //                     <NavLink className="nav-link" to="/"><li className="nav-item">Home</li></NavLink>
-        //                     <NavLink className="nav-link" to="/about"><li className="nav-item">About</li></NavLink>
-        //                     <NavLink className="nav-link" to="/contact"><li className="nav-item">Contact</li></NavLink>
-        //                     <NavLink className="nav-link" to="/menu"><li className="nav-item">Menu</li></NavLink>
-        //                     <NavLink className="nav-link" to="/reservation"><li className="nav-item">Reservation</li></NavLink>
-        //                 {user && (
-        //                     <NavLink className="nav-link" to="/new"><li className="nav-item">New Article</li></NavLink>
-        //                 )}
-        //                 {!user && (
-        //                     <>
-        //                         <NavLink className="nav-link" to="/login"><li className="nav-item">Login</li></NavLink>
-        //                         <NavLink className="nav-link" to="/signup"><li className="nav-item">Signup</li></NavLink>
-        //                     </>
-        //                 )}
-        //                 {user && (
-        //                     <>
-        //                         <li className="nav-item">
-        //                             <span className="navbar-text mx-2">Hello, {user.displayName}</span>
-        //                         </li>
-        //                         <li className="nav-item">
-        //                             <button className="btn btn-sm custom-logout-btn" onClick={handleLogout}>Logout</button>
-        //                         </li>
-        //                     </>
-        //                 )}
-        //             </ul>
-        //         </div>
-
-        //     </div>
-        // </nav>
     );
 }
